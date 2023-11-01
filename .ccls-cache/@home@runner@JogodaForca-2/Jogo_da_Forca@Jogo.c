2d648b30f1f31c5448cc52c62b6687cca6e85f46
Christiele123 <142457805+Christiele123@users.noreply.github.com>
2023-11-01 23:45:34 +0000

    ..

--- .ccls-cache/@home@runner@JogodaForca-2/Jogo_da_Forca@Jogo.c
+++ .ccls-cache/@home@runner@JogodaForca-2/Jogo_da_Forca@Jogo.c
@@ -4,38 +4,32 @@
 int main() {
   char opcao = 0;
   float saldo = 0.0f;
-  int tentativas = 3, acertos = 0,letras_acertadas = 0, letras_erradas = 3;
-  char palavra[10]
-  char resposta[10]
+  int numeros[10];
+  char palavra[20];
+  int tentativa = 3;
   
-  
+
   while (opcao != '0') {
     int deu_certo = system("clear");
     printf("\e[0; 96mJOGO DA FORCA\e[0m\n");
     printf("1 - NOVO JOGO\n");
-    printf("2 - CARREGAR JOGO\n");
+    printf("2 - RANKING\n");
     printf("3 - SAIR\n");
     deu_certo = scanf("%c", &opcao);
-    while (getchar() != '\n'){
-      switch (opcao) {
-        case '1':
-          deu_certo = system("clear");
-          printf("NOVO JOGO\n");
-          printf("1 - FÁCIL\n");
-          printf("2 - MÉDIO\n");
-          printf("3 - DIFÍCIL\n");
-          printf("0 - SAIR\n");
-          deu_certo = scanf("%c", &valor);
-          while (getchar() != '\n');
-        switch (valor) {
-          case '1':
-          
-          
-          
-        }
-        
-        
-        
+    while (getchar() != '\n') {
     }
-  }
-  return 0;
+    switch (opcao) {
+    case '1':
+      deu_certo = system("clear");
+      printf("NOVO JOGO\n");
+      printf("1 - FÁCIL\n");
+      printf("2 - MÉDIO\n");
+      printf("3 - DIFÍCIL\n");
+      printf("0 - SAIR\n");
+      deu_certo = scanf("%c", &opcao);
+      while (getchar() != '\n');
+      switch (opcao) {
+    }
+    
+}
+return 0;
