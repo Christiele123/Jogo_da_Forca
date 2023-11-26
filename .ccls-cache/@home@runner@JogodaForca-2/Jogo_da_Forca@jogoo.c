d3761222e0bc57d5daae4b528d0a74ec38c542f2
Christiele123 <142457805+Christiele123@users.noreply.github.com>
2023-11-26 01:30:37 +0000

    DEU CERTO

--- .ccls-cache/@home@runner@JogodaForca-2/Jogo_da_Forca@jogoo.c
+++ .ccls-cache/@home@runner@JogodaForca-2/Jogo_da_Forca@jogoo.c
@@ -4,33 +4,60 @@
 #include <string.h>
 #include <time.h>
 
-int main();{
+#define MAX_TENTATIVAS_FACIL 8
+#define MAX_TENTATIVAS_MEDIO 6
+#define MAX_TENTATIVAS_DIFICIL 4
+#define NUM_PALAVRAS_FACIL 10
+#define NUM_PALAVRAS_MEDIO 10
+#define NUM_PALAVRAS_DIFICIL 10
+#define PONTUACAO_POR_PALAVRA_FACIL 5
+#define PONTUACAO_POR_PALAVRA_MEDIO 10
+#define PONTUACAO_POR_PALAVRA_DIFICIL 15
+#define MAX_JOGADORES 5
 
-#define MAX_TENTATIVAS 6
-#define NUM_PALAVRAS 5
+char *palavrasFacil[NUM_PALAVRAS_FACIL] = {"CASA", "GATO", "AMOR", "PASTAS",
+                                           "FICA", "SEIS", "BOLA", "JOIA",
+                                           "FIAT", "SAPO"};
+char *palavrasMedio[NUM_PALAVRAS_MEDIO] = {"RECEIO", "INSENTO", "PAIXÃO",  "REINOS",
+                                           "CASUAL", "ANSEIO", "APREÇO", "RANCOR",
+                                           "TETO", "ALENTO"};
+char *palavrasDificil[NUM_PALAVRAS_DIFICIL] = {
+    "DEMOCRACIA", "COMPUTADOR", "HIPOTETICO", "DESENVOLVIMENTO", "ALGORITMO",
+    "CACHORRO",    "ELEFANTE",   "MODALIDADE", "CONSTRUCAO",      "ENGENHARIA"};
 
-char *palavras[NUM_PALAVRAS] = {"PROGRAMACAO", "COMPUTADOR", "LINGUAGEM",
-                                "DESENVOLVIMENTO", "ALGORITMO"};
+typedef struct {
+  char nome[50];
+  int pontuacao;
+} Jogador;
+
+void esperar(unsigned int segundos) {
+  unsigned int tempoFim = time(NULL) + segundos;
+  while (time(NULL) < tempoFim)
+    ;
+}
 
 void inicializarForca(char *palavraSecreta, char *letrasReveladas) {
-  int i;
-  for (i = 0; i < strlen(palavraSecreta); i++) {
+  for (int i = 0; i < strlen(palavraSecreta); i++) {
     if (palavraSecreta[i] == ' ')
       letrasReveladas[i] = ' ';
     else
       letrasReveladas[i] = '_';
   }
-  letrasReveladas[i] = '\0';
+  letrasReveladas[strlen(palavraSecreta)] = '\0';
 }
 
-void exibirForca(char *letrasReveladas) {
-  printf("Palavra: %s\n", letrasReveladas);
+void exibirForca(char *letrasReveladas, int tentativas, int maxTentativas) {
+  printf("\e[46m\e[0;95m+---------------------------------------------------------------------------------+\n");
+  printf("| Palavra: %-12s                                                            |\n", letrasReveladas);
+  printf("| Tentativas restantes: %-3d/%-3d                                                    |\n",
+         maxTentativas - tentativas, maxTentativas);
+  printf("+----------------------------------------------------------------------------------+\n");
 }
 
 int letraEstaNaPalavra(char letra, char *palavraSecreta,
                        char *letrasReveladas) {
-  int i, encontrada = 0;
-  for (i = 0; i < strlen(palavraSecreta); i++) {
+  int encontrada = 0;
+  for (int i = 0; i < strlen(palavraSecreta); i++) {
     if (palavraSecreta[i] == letra) {
       letrasReveladas[i] = letra;
       encontrada = 1;
@@ -39,48 +66,128 @@ int letraEstaNaPalavra(char letra, char *palavraSecreta,
   return encontrada;
 }
 
-void exibirMenu() {
-  printf("\nMENU:\n");
-  printf("1 - Novo Jogo\n");
-  printf("0 - Sair\n");
+void exibirRanking(Jogador *ranking, int numJogadores) {
+  printf("\e[46m\e[0;95m+-----------------------------------------------------------------------------------+\n");
+  printf("|                                       RANKING                                      |\n");
+  printf("+------------------------------------------------------------------------------------+\n");
+  printf("|   Posição   |                    Nome               |           Pontuação          |\n");
+  printf("-------------------------------------------------------------------------------------+\n");
+  for (int i = 0; i < numJogadores; i++) {
+    printf("|      %-4d   |                    %-8s           | %-17d            |\n", i + 1, ranking[i].nome,
+           ranking[i].pontuacao);
+  }
+  printf("+------------------------------------------------------------------------------------+\n");
+}
+
+void adicionarAoRanking(Jogador *ranking, int *numJogadores,
+                        Jogador novoJogador) {
+  for (int i = 0; i < *numJogadores; i++) {
+    if (strcmp(ranking[i].nome, novoJogador.nome) == 0) {
+      ranking[i].pontuacao += novoJogador.pontuacao;
+      return;
+    }
+  }
+
+  if (*numJogadores < MAX_JOGADORES) {
+    ranking[*numJogadores] = novoJogador;
+    (*numJogadores)++;
+  } else {
+    int piorPontuacao = ranking[0].pontuacao;
+    int indicePiorJogador = 0;
+
+    for (int i = 1; i < MAX_JOGADORES; i++) {
+      if (ranking[i].pontuacao < piorPontuacao) {
+        piorPontuacao = ranking[i].pontuacao;
+        indicePiorJogador = i;
+      }
+    }
+
+    if (novoJogador.pontuacao > piorPontuacao) {
+      ranking[indicePiorJogador] = novoJogador;
+    }
+  }
 }
 
 int main() {
   srand(time(NULL));
-
+  
   char palavraSecreta[20];
   char letrasReveladas[20];
   char letra;
   int tentativas;
+  int escolhaMenu;
+  int maxTentativas;
+  int pontuacaoTotal = 0;
 
-  int opcao;
+  Jogador ranking[MAX_JOGADORES];
+  int numJogadores = 0;
 
   do {
-    system("clear");
-    exibirMenu();
+    int ok = system("clear");
+    printf("\e[46m\e[0;95m+--------------------------------------------------------------------------------+\n");
+    printf("|                                  JOGO DA FORCA                                 |\n");
+    printf("+--------------------------------------------------------------------------------+\n");
+    printf("\e[46m\e[0;95m+--------------------------------------------------------------------------------+\n");
+    printf("|                                    MENU                                        |\n");
+    printf("+--------------------------------------------------------------------------------+\n");
+    printf("|                                  1 - Novo Jogo                                 |\n");
+    printf("|                                  2 - Ranking                                   |\n");
+    printf("|                                  3 - Sair                                      |\n");
+    printf("+-------------------------------------------------------------------------------+\n");
+    printf("Escolha uma opção: \e[0m");
+    ok = scanf("%d", &escolhaMenu);
 
-    printf("Escolha uma opção: ");
-    scanf("%d", &opcao);
-
-    switch (opcao) {
+    switch (escolhaMenu) {
     case 1:
+      ok = system("clear");
+
+      printf("\e[46m\e[0;95m+----------------------------------------------------------------------------------+\n");
+      printf("|                                NÍVEIS DE DIFICULDADE                             |\n");
+      printf("+----------------------------------------------------------------------------------+\n");
+      printf("|                                 1 - Fácil                                        |\n");
+      printf("|                                 2 - Médio                                        |\n");
+      printf("|                                 3 - Difícil                                      |\n");
+      printf("+---------------------------------------------------------------------------------+\n");
+      printf("Escolha o nível de dificuldade:\e[0m ");
+      int nivel;
+      ok = scanf("%d", &nivel);
+
+      switch (nivel) {
+      case 1:
+        maxTentativas = MAX_TENTATIVAS_FACIL;
+        strcpy(palavraSecreta, palavrasFacil[rand() % NUM_PALAVRAS_FACIL]);
+        pontuacaoTotal += PONTUACAO_POR_PALAVRA_FACIL;
+        break;
+      case 2:
+        maxTentativas = MAX_TENTATIVAS_MEDIO;
+        strcpy(palavraSecreta, palavrasMedio[rand() % NUM_PALAVRAS_MEDIO]);
+        pontuacaoTotal += PONTUACAO_POR_PALAVRA_MEDIO;
+        break;
+      case 3:
+        maxTentativas = MAX_TENTATIVAS_DIFICIL;
+        strcpy(palavraSecreta,
+               palavrasDificil[rand() % NUM_PALAVRAS_DIFICIL]);
+        pontuacaoTotal += PONTUACAO_POR_PALAVRA_DIFICIL;
+        break;
+      default:
+        printf("\e[46m\e[0;95mOpção inválida. Voltando ao menu.\n");
+        continue;
+      }
       tentativas = 0;
-      strcpy(palavraSecreta, palavras[rand() % NUM_PALAVRAS]);
       inicializarForca(palavraSecreta, letrasReveladas);
 
       while (1) {
-        system("clear");
+        ok = system("clear");
 
-        exibirForca(letrasReveladas);
-        printf("Tentativas restantes: %d\n", MAX_TENTATIVAS - tentativas);
+        exibirForca(letrasReveladas, tentativas, maxTentativas);
 
-        printf("Digite uma letra: ");
-        scanf(" %c", &letra);
-
-        letra = toupper(letra); // Converter para maiúscula
+        printf("\e[46m\e[0;95mDigite uma letra: ");
+        ok = scanf(" %c", &letra);
+        getchar();
+        letra = toupper(letra);
 
         if (!isalpha(letra)) {
-          printf("Por favor, digite uma letra válida.\n");
+          printf("\e[46m\e[0;95mPor favor, digite uma letra válida.\n");
           continue;
         }
 
@@ -89,28 +196,61 @@ int main() {
         }
 
         if (strcmp(palavraSecreta, letrasReveladas) == 0) {
-          system("clear");
-          exibirForca(letrasReveladas);
-          printf("Parabéns! Você acertou a palavra: %s\n", palavraSecreta);
+          ok = system("clear");
+          exibirForca(letrasReveladas, tentativas, maxTentativas);
+          printf("\e[46m\e[0;95mParabéns! Você acertou a palavra: %s\n", palavraSecreta);
+          Jogador novoJogador;
+          printf("\e[46m\e[0;95mDigite seu nome para o ranking: ");
+          ok = scanf("%s", novoJogador.nome);
+          getchar();
+          novoJogador.pontuacao = pontuacaoTotal;
+          adicionarAoRanking(ranking, &numJogadores, novoJogador);
+          esperar(2);
           break;
         }
 
-        if (tentativas == MAX_TENTATIVAS) {
-          system("clear");
-          printf("Você perdeu! A palavra era: %s\n", palavraSecreta);
+        if (tentativas == maxTentativas) {
+          ok = system("clear");
+          printf("\e[46m\e[0;95mVocê perdeu! A palavra era: %s\n", palavraSecreta);
+          printf("\e[46m\e[0;95mSua pontuação: %d\n", pontuacaoTotal);
+          esperar(2);
           break;
         }
       }
       break;
-    case 0:
-      printf("Saindo do jogo.\n");
+    case 2:
+      do {
+        ok = system("clear");
+
+        exibirRanking(ranking, numJogadores);
+
+        printf("\e[46m\e[0;95m+------------------------------------------------------------------------------------+\n");
+        printf("\e[46m\e[0;95m+------------------------------------------------------------------------------------+\n");
+        printf("|                             4 - Voltar ao Menu                                     |\n");
+        printf("|                             5 - Sair do Jogo                                       |\n");
+        printf("+-----------------------------------------------------------------------------------+\n");
+        printf("\e[46m\e[0;95mEscolha uma opção: ");
+        ok = scanf("%d", &escolhaMenu);
+
+        if (escolhaMenu == 4) {
+          break; // Voltar ao Menu
+        } else if (escolhaMenu == 5) {
+          printf("\e[46m\e[0;95mSaindo do jogo.\n");
+          return 0;
+        } else {
+          printf("\e[46m\e[0;95mOpção inválida. Tente novamente.\n");
+        }
+      } while (1);
+      break;
+    case 3:
+      printf("\e[46m\e[0;95mSaindo do jogo.\n");
       break;
     default:
-      printf("Opção inválida. Tente novamente.\n");
+      printf("\e[46m\e[0;95mOpção inválida. Tente novamente.\n");
       break;
     }
 
-  } while (opcao != 0);
+  } while (escolhaMenu != 3);
 
   return 0;
 }
--- Jogo_da_Forca/jogoo.c
+++ Jogo_da_Forca/jogoo.c
@@ -1,149 +1,256 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <time.h>
 
 #define MAX_TENTATIVAS_FACIL 8
 #define MAX_TENTATIVAS_MEDIO 6
 #define MAX_TENTATIVAS_DIFICIL 4
-#define NUM_PALAVRAS 5
+#define NUM_PALAVRAS_FACIL 10
+#define NUM_PALAVRAS_MEDIO 10
+#define NUM_PALAVRAS_DIFICIL 10
+#define PONTUACAO_POR_PALAVRA_FACIL 5
+#define PONTUACAO_POR_PALAVRA_MEDIO 10
+#define PONTUACAO_POR_PALAVRA_DIFICIL 15
+#define MAX_JOGADORES 5
 
-char* palavras[NUM_PALAVRAS] = {"PROGRAMACAO", "COMPUTADOR", "LINGUAGEM", "DESENVOLVIMENTO", "ALGORITMO"};
+char *palavrasFacil[NUM_PALAVRAS_FACIL] = {"CASA", "GATO", "AMOR", "PASTAS",
+                                           "FICA", "SEIS", "BOLA", "JOIA",
+                                           "FIAT", "SAPO"};
+char *palavrasMedio[NUM_PALAVRAS_MEDIO] = {"RECEIO", "INSENTO", "PAIXÃO",  "REINOS",
+                                           "CASUAL", "ANSEIO", "APREÇO", "RANCOR",
+                                           "TETO", "ALENTO"};
+char *palavrasDificil[NUM_PALAVRAS_DIFICIL] = {
+    "DEMOCRACIA", "COMPUTADOR", "HIPOTETICO", "DESENVOLVIMENTO", "ALGORITMO",
+    "CACHORRO",    "ELEFANTE",   "MODALIDADE", "CONSTRUCAO",      "ENGENHARIA"};
 
-void inicializarForca(char* palavraSecreta, char* letrasReveladas) {
-    for (int i = 0; i < strlen(palavraSecreta); i++) {
-        if (palavraSecreta[i] == ' ')
-            letrasReveladas[i] = ' ';
-        else
-            letrasReveladas[i] = '_';
-    }
-    letrasReveladas[strlen(palavraSecreta)] = '\0';
+typedef struct {
+  char nome[50];
+  int pontuacao;
+} Jogador;
+
+void esperar(unsigned int segundos) {
+  unsigned int tempoFim = time(NULL) + segundos;
+  while (time(NULL) < tempoFim)
+    ;
 }
 
-void exibirForca(char* letrasReveladas, int tentativas, int maxTentativas) {
-    printf("Palavra: %s\n", letrasReveladas);
-    printf("Tentativas restantes: %d/%d\n", maxTentativas - tentativas, maxTentativas);
+void inicializarForca(char *palavraSecreta, char *letrasReveladas) {
+  for (int i = 0; i < strlen(palavraSecreta); i++) {
+    if (palavraSecreta[i] == ' ')
+      letrasReveladas[i] = ' ';
+    else
+      letrasReveladas[i] = '_';
+  }
+  letrasReveladas[strlen(palavraSecreta)] = '\0';
 }
 
-int letraEstaNaPalavra(char letra, char* palavraSecreta, char* letrasReveladas) {
-    int encontrada = 0;
-    for (int i = 0; i < strlen(palavraSecreta); i++) {
-        if (palavraSecreta[i] == letra) {
-            letrasReveladas[i] = letra;
-            encontrada = 1;
-        }
+void exibirForca(char *letrasReveladas, int tentativas, int maxTentativas) {
+  printf("\e[46m\e[0;95m+---------------------------------------------------------------------------------+\n");
+  printf("| Palavra: %-12s                                                            |\n", letrasReveladas);
+  printf("| Tentativas restantes: %-3d/%-3d                                                    |\n",
+         maxTentativas - tentativas, maxTentativas);
+  printf("+----------------------------------------------------------------------------------+\n");
+}
+
+int letraEstaNaPalavra(char letra, char *palavraSecreta,
+                       char *letrasReveladas) {
+  int encontrada = 0;
+  for (int i = 0; i < strlen(palavraSecreta); i++) {
+    if (palavraSecreta[i] == letra) {
+      letrasReveladas[i] = letra;
+      encontrada = 1;
     }
-    return encontrada;
+  }
+  return encontrada;
+}
+
+void exibirRanking(Jogador *ranking, int numJogadores) {
+  printf("\e[46m\e[0;95m+-----------------------------------------------------------------------------------+\n");
+  printf("|                                       RANKING                                      |\n");
+  printf("+------------------------------------------------------------------------------------+\n");
+  printf("|   Posição   |                    Nome               |           Pontuação          |\n");
+  printf("-------------------------------------------------------------------------------------+\n");
+  for (int i = 0; i < numJogadores; i++) {
+    printf("|      %-4d   |                    %-8s           | %-17d            |\n", i + 1, ranking[i].nome,
+           ranking[i].pontuacao);
+  }
+  printf("+------------------------------------------------------------------------------------+\n");
+}
+
+void adicionarAoRanking(Jogador *ranking, int *numJogadores,
+                        Jogador novoJogador) {
+  for (int i = 0; i < *numJogadores; i++) {
+    if (strcmp(ranking[i].nome, novoJogador.nome) == 0) {
+      ranking[i].pontuacao += novoJogador.pontuacao;
+      return;
+    }
+  }
+
+  if (*numJogadores < MAX_JOGADORES) {
+    ranking[*numJogadores] = novoJogador;
+    (*numJogadores)++;
+  } else {
+    int piorPontuacao = ranking[0].pontuacao;
+    int indicePiorJogador = 0;
+
+    for (int i = 1; i < MAX_JOGADORES; i++) {
+      if (ranking[i].pontuacao < piorPontuacao) {
+        piorPontuacao = ranking[i].pontuacao;
+        indicePiorJogador = i;
+      }
+    }
+
+    if (novoJogador.pontuacao > piorPontuacao) {
+      ranking[indicePiorJogador] = novoJogador;
+    }
+  }
 }
 
 int main() {
-    srand(time(NULL));
+  srand(time(NULL));
+  
+  char palavraSecreta[20];
+  char letrasReveladas[20];
+  char letra;
+  int tentativas;
+  int escolhaMenu;
+  int maxTentativas;
+  int pontuacaoTotal = 0;
 
-    char palavraSecreta[20];
-    char letrasReveladas[20];
-    char letra;
-    int tentativas;
-    int opcao;
-    int maxTentativas;
+  Jogador ranking[MAX_JOGADORES];
+  int numJogadores = 0;
 
-    do {
-        int ok =system("clear");
+  do {
+    int ok = system("clear");
+    printf("\e[46m\e[0;95m+--------------------------------------------------------------------------------+\n");
+    printf("|                                  JOGO DA FORCA                                 |\n");
+    printf("+--------------------------------------------------------------------------------+\n");
+    printf("\e[46m\e[0;95m+--------------------------------------------------------------------------------+\n");
+    printf("|                                    MENU                                        |\n");
+    printf("+--------------------------------------------------------------------------------+\n");
+    printf("|                                  1 - Novo Jogo                                 |\n");
+    printf("|                                  2 - Ranking                                   |\n");
+    printf("|                                  3 - Sair                                      |\n");
+    printf("+-------------------------------------------------------------------------------+\n");
+    printf("Escolha uma opção: \e[0m");
+    ok = scanf("%d", &escolhaMenu);
 
-        printf("\nMENU:\n");
-        printf("1 - Novo Jogo\n");
-        printf("2 - Ranking\n");
-        printf("3 - Sair\n");
-        printf("Escolha uma opção: ");
-        ok = scanf("%d", &opcao);
+    switch (escolhaMenu) {
+    case 1:
+      ok = system("clear");
 
-        switch (opcao) {
-            case 1:
-                ok = system("clear");
+      printf("\e[46m\e[0;95m+----------------------------------------------------------------------------------+\n");
+      printf("|                                NÍVEIS DE DIFICULDADE                             |\n");
+      printf("+----------------------------------------------------------------------------------+\n");
+      printf("|                                 1 - Fácil                                        |\n");
+      printf("|                                 2 - Médio                                        |\n");
+      printf("|                                 3 - Difícil                                      |\n");
+      printf("+---------------------------------------------------------------------------------+\n");
+      printf("Escolha o nível de dificuldade:\e[0m ");
+      int nivel;
+      ok = scanf("%d", &nivel);
 
-                printf("\nNÍVEIS DE DIFICULDADE:\n");
-                printf("1 - Fácil\n");
-                printf("2 - Médio\n");
-                printf("3 - Difícil\n");
-                printf("Escolha o nível de dificuldade: ");
-                int nivel;
-                ok = scanf("%d", &nivel);
+      switch (nivel) {
+      case 1:
+        maxTentativas = MAX_TENTATIVAS_FACIL;
+        strcpy(palavraSecreta, palavrasFacil[rand() % NUM_PALAVRAS_FACIL]);
+        pontuacaoTotal += PONTUACAO_POR_PALAVRA_FACIL;
+        break;
+      case 2:
+        maxTentativas = MAX_TENTATIVAS_MEDIO;
+        strcpy(palavraSecreta, palavrasMedio[rand() % NUM_PALAVRAS_MEDIO]);
+        pontuacaoTotal += PONTUACAO_POR_PALAVRA_MEDIO;
+        break;
+      case 3:
+        maxTentativas = MAX_TENTATIVAS_DIFICIL;
+        strcpy(palavraSecreta,
+               palavrasDificil[rand() % NUM_PALAVRAS_DIFICIL]);
+        pontuacaoTotal += PONTUACAO_POR_PALAVRA_DIFICIL;
+        break;
+      default:
+        printf("\e[46m\e[0;95mOpção inválida. Voltando ao menu.\n");
+        continue;
+      }
+      tentativas = 0;
+      inicializarForca(palavraSecreta, letrasReveladas);
 
-                switch (nivel) {
-                    case 1:
-                        maxTentativas = MAX_TENTATIVAS_FACIL;
-                        break;
-                    case 2:
-                        maxTentativas = MAX_TENTATIVAS_MEDIO;
-                        break;
-                    case 3:
-                        maxTentativas = MAX_TENTATIVAS_DIFICIL;
-                        break;
-                    default:
-                        printf("Opção inválida. Voltando ao menu.\n");
-                        continue;
-                }
+      while (1) {
+        ok = system("clear");
 
-                tentativas = 0;
-                strcpy(palavraSecreta, palavras[rand() % NUM_PALAVRAS]);
-                inicializarForca(palavraSecreta, letrasReveladas);
+        exibirForca(letrasReveladas, tentativas, maxTentativas);
 
-                while (1) {
-                    ok = system("clear");
+        printf("\e[46m\e[0;95mDigite uma letra: ");
+        ok = scanf(" %c", &letra);
+        getchar();
+        letra = toupper(letra);
 
-                    exibirForca(letrasReveladas, tentativas, maxTentativas);
-
-                    printf("Digite uma letra: ");
-                    ok = scanf(" %c", &letra);
-                    getchar();
-                  
-                    letra = toupper(letra);  // Converter para maiúscula
-
-                    if (!isalpha(letra)) {
-                        printf("Por favor, digite uma letra válida.\n");
-                        continue;
-                    }
-
-                    if (!letraEstaNaPalavra(letra, palavraSecreta, letrasReveladas)) {
-                        tentativas++;
-                    }
-
-                    if (strcmp(palavraSecreta, letrasReveladas) == 0) {
-                        ok = system("clear");
-                        exibirForca(letrasReveladas, tentativas, maxTentativas);
-                        printf("Parabéns! Você acertou a palavra: %s\n", palavraSecreta);
-                      getchar();
-                        break;
-                    }
-
-                    if (tentativas == maxTentativas) {
-                        ok = system("clear");
-                        printf("Você perdeu! A palavra era: %s\n", palavraSecreta);
-                      getchar();
-                        break;
-                    }
-                }
-                break;
-            case 2:
-              ok = system("clear");
-              printf("Ranking:\n");
-              printf("1 - Jogador 1: XXX pontos\n");
-              printf("2 - Jogador 2: XXX pontos\n");
-              printf("3 - Jogador 3: XXX pontos\n");
-              printf("4 - Sair\n");
-              break;
-                printf("Saindo do jogo.\n");
-                break;
-            default:
-                printf("Opção inválida. Tente novamente.\n");
-                break;
-          case 3:
-            printf("Saindo do jogo.\n");
-            break;
+        if (!isalpha(letra)) {
+          printf("\e[46m\e[0;95mPor favor, digite uma letra válida.\n");
+          continue;
         }
 
-    } while (opcao != 2);
+        if (!letraEstaNaPalavra(letra, palavraSecreta, letrasReveladas)) {
+          tentativas++;
+        }
 
-    return 0;
+        if (strcmp(palavraSecreta, letrasReveladas) == 0) {
+          ok = system("clear");
+          exibirForca(letrasReveladas, tentativas, maxTentativas);
+          printf("\e[46m\e[0;95mParabéns! Você acertou a palavra: %s\n", palavraSecreta);
+          Jogador novoJogador;
+          printf("\e[46m\e[0;95mDigite seu nome para o ranking: ");
+          ok = scanf("%s", novoJogador.nome);
+          getchar();
+          novoJogador.pontuacao = pontuacaoTotal;
+          adicionarAoRanking(ranking, &numJogadores, novoJogador);
+          esperar(2);
+          break;
+        }
+
+        if (tentativas == maxTentativas) {
+          ok = system("clear");
+          printf("\e[46m\e[0;95mVocê perdeu! A palavra era: %s\n", palavraSecreta);
+          printf("\e[46m\e[0;95mSua pontuação: %d\n", pontuacaoTotal);
+          esperar(2);
+          break;
+        }
+      }
+      break;
+    case 2:
+      do {
+        ok = system("clear");
+
+        exibirRanking(ranking, numJogadores);
+
+        printf("\e[46m\e[0;95m+------------------------------------------------------------------------------------+\n");
+        printf("\e[46m\e[0;95m+------------------------------------------------------------------------------------+\n");
+        printf("|                             4 - Voltar ao Menu                                     |\n");
+        printf("|                             5 - Sair do Jogo                                       |\n");
+        printf("+-----------------------------------------------------------------------------------+\n");
+        printf("\e[46m\e[0;95mEscolha uma opção: ");
+        ok = scanf("%d", &escolhaMenu);
+
+        if (escolhaMenu == 4) {
+          break; // Voltar ao Menu
+        } else if (escolhaMenu == 5) {
+          printf("\e[46m\e[0;95mSaindo do jogo.\n");
+          return 0;
+        } else {
+          printf("\e[46m\e[0;95mOpção inválida. Tente novamente.\n");
+        }
+      } while (1);
+      break;
+    case 3:
+      printf("\e[46m\e[0;95mSaindo do jogo.\n");
+      break;
+    default:
+      printf("\e[46m\e[0;95mOpção inválida. Tente novamente.\n");
+      break;
+    }
+
+  } while (escolhaMenu != 3);
+
+  return 0;
 }
-
