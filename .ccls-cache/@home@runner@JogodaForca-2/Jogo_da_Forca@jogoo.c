dc24e73fe82ba91153cb3f618ea9c96506f1ed18
Christiele123 <142457805+Christiele123@users.noreply.github.com>
2023-11-27 02:20:32 +0000

    deu certo

--- .ccls-cache/@home@runner@JogodaForca-2/Jogo_da_Forca@jogoo.c
+++ .ccls-cache/@home@runner@JogodaForca-2/Jogo_da_Forca@jogoo.c
@@ -1,9 +1,11 @@
+// Inclusão de Bibliotecas Necessárias
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
+// Constantes que definem as regras do jogo
 #define MAX_TENTATIVAS_FACIL 8
 #define MAX_TENTATIVAS_MEDIO 6
 #define MAX_TENTATIVAS_DIFICIL 4
@@ -15,27 +17,25 @@
 #define PONTUACAO_POR_PALAVRA_DIFICIL 15
 #define MAX_JOGADORES 5
 
-char *palavrasFacil[NUM_PALAVRAS_FACIL] = {"CASA", "GATO", "AMOR", "PASTAS",
-                                           "FICA", "SEIS", "BOLA", "JOIA",
-                                           "FIAT", "SAPO"};
-char *palavrasMedio[NUM_PALAVRAS_MEDIO] = {"RECEIO", "INSENTO", "PAIXÃO",  "REINOS",
-                                           "CASUAL", "ANSEIO", "APREÇO", "RANCOR",
-                                           "TETO", "ALENTO"};
-char *palavrasDificil[NUM_PALAVRAS_DIFICIL] = {
-    "DEMOCRACIA", "COMPUTADOR", "HIPOTETICO", "DESENVOLVIMENTO", "ALGORITMO",
-    "CACHORRO",    "ELEFANTE",   "MODALIDADE", "CONSTRUCAO",      "ENGENHARIA"};
+// Palavras para cada nível de dificuldade
+char *palavrasFacil[NUM_PALAVRAS_FACIL] = {"CASA", "GATO", "AMOR", "PASTA", "FICA", "SEIS", "BOLA", "JOIA", "FIAT", "SAPO"};
+char *palavrasMedio[NUM_PALAVRAS_MEDIO] = {"RECEIO", "INSENTO", "PAIXÃO", "REINOS", "CASUAL", "ANSEIO", "APREÇO", "RANCOR", "TETO", "ALENTO"};
+char *palavrasDificil[NUM_PALAVRAS_DIFICIL] = {"DEMOCRACIA", "COMPUTADOR", "HIPOTETICO", "DESENVOLVIMENTO", "ALGORITMO", "CACHORRO", "ELEFANTE", "MODALIDADE", "CONSTRUCAO", "ENGENHARIA"};
 
+// Estrutura que representa um jogador
 typedef struct {
-  char nome[50];
+  char nome[6];  // Limitado a 5 caracteres + 1 para o caractere nulo '\0'
   int pontuacao;
 } Jogador;
 
+// Função que pausa a execução por um número de segundos
 void esperar(unsigned int segundos) {
   unsigned int tempoFim = time(NULL) + segundos;
   while (time(NULL) < tempoFim)
     ;
 }
 
+// Inicializa as letras reveladas para a palavra secreta
 void inicializarForca(char *palavraSecreta, char *letrasReveladas) {
   for (int i = 0; i < strlen(palavraSecreta); i++) {
     if (palavraSecreta[i] == ' ')
@@ -46,6 +46,7 @@ void inicializarForca(char *palavraSecreta, char *letrasReveladas) {
   letrasReveladas[strlen(palavraSecreta)] = '\0';
 }
 
+// Exibe o estado atual do jogo (forca)
 void exibirForca(char *letrasReveladas, int tentativas, int maxTentativas) {
   printf("\e[46m\e[0;95m+---------------------------------------------------------------------------------+\n");
   printf("| Palavra: %-12s                                                            |\n", letrasReveladas);
@@ -54,8 +55,8 @@ void exibirForca(char *letrasReveladas, int tentativas, int maxTentativas) {
   printf("+----------------------------------------------------------------------------------+\n");
 }
 
-int letraEstaNaPalavra(char letra, char *palavraSecreta,
-                       char *letrasReveladas) {
+// Verifica se uma letra está na palavra secreta e a revela
+int letraEstaNaPalavra(char letra, char *palavraSecreta, char *letrasReveladas) {
   int encontrada = 0;
   for (int i = 0; i < strlen(palavraSecreta); i++) {
     if (palavraSecreta[i] == letra) {
@@ -66,6 +67,7 @@ int letraEstaNaPalavra(char letra, char *palavraSecreta,
   return encontrada;
 }
 
+// Exibe o ranking de jogadores
 void exibirRanking(Jogador *ranking, int numJogadores) {
   printf("\e[46m\e[0;95m+-----------------------------------------------------------------------------------+\n");
   printf("|                                       RANKING                                      |\n");
@@ -73,14 +75,14 @@ void exibirRanking(Jogador *ranking, int numJogadores) {
   printf("|   Posição   |                    Nome               |           Pontuação          |\n");
   printf("-------------------------------------------------------------------------------------+\n");
   for (int i = 0; i < numJogadores; i++) {
-    printf("|      %-4d   |                    %-8s           | %-17d            |\n", i + 1, ranking[i].nome,
+    printf("|      %-4d   |                   %-8s            |             %-17d|\n", i + 1, ranking[i].nome,
            ranking[i].pontuacao);
   }
   printf("+------------------------------------------------------------------------------------+\n");
 }
 
-void adicionarAoRanking(Jogador *ranking, int *numJogadores,
-                        Jogador novoJogador) {
+// Adiciona um novo jogador ao ranking ou atualiza a pontuação se já existir
+void adicionarAoRanking(Jogador *ranking, int *numJogadores, Jogador novoJogador) {
   for (int i = 0; i < *numJogadores; i++) {
     if (strcmp(ranking[i].nome, novoJogador.nome) == 0) {
       ranking[i].pontuacao += novoJogador.pontuacao;
@@ -89,12 +91,15 @@ void adicionarAoRanking(Jogador *ranking, int *numJogadores,
   }
 
   if (*numJogadores < MAX_JOGADORES) {
-    ranking[*numJogadores] = novoJogador;
+    // Adiciona um novo jogador se houver espaço no ranking
+    strncpy(ranking[*numJogadores].nome, novoJogador.nome, 5);
+    ranking[*numJogadores].nome[5] = '\0';  // Certifica-se de terminar a string
+    ranking[*numJogadores].pontuacao = novoJogador.pontuacao;
     (*numJogadores)++;
   } else {
+    // Encontra o jogador com a pior pontuação no ranking
     int piorPontuacao = ranking[0].pontuacao;
     int indicePiorJogador = 0;
-
     for (int i = 1; i < MAX_JOGADORES; i++) {
       if (ranking[i].pontuacao < piorPontuacao) {
         piorPontuacao = ranking[i].pontuacao;
@@ -102,15 +107,19 @@ void adicionarAoRanking(Jogador *ranking, int *numJogadores,
       }
     }
 
+    // Substitui o pior jogador se a pontuação do novo jogador for melhor
     if (novoJogador.pontuacao > piorPontuacao) {
-      ranking[indicePiorJogador] = novoJogador;
+      strncpy(ranking[indicePiorJogador].nome, novoJogador.nome, 5);
+      ranking[indicePiorJogador].nome[5] = '\0';  // Certifica-se de terminar a string
+      ranking[indicePiorJogador].pontuacao = novoJogador.pontuacao;
     }
   }
 }
 
+// Função principal que controla o fluxo do jogo
 int main() {
   srand(time(NULL));
-  
+
   char palavraSecreta[20];
   char letrasReveladas[20];
   char letra;
@@ -124,6 +133,8 @@ int main() {
 
   do {
     int ok = system("clear");
+
+    // Exibe o menu principal
     printf("\e[46m\e[0;95m+--------------------------------------------------------------------------------+\n");
     printf("|                                  JOGO DA FORCA                                 |\n");
     printf("+--------------------------------------------------------------------------------+\n");
@@ -141,6 +152,7 @@ int main() {
     case 1:
       ok = system("clear");
 
+      // Exibe o menu de seleção de dificuldade
       printf("\e[46m\e[0;95m+----------------------------------------------------------------------------------+\n");
       printf("|                                NÍVEIS DE DIFICULDADE                             |\n");
       printf("+----------------------------------------------------------------------------------+\n");
@@ -165,8 +177,7 @@ int main() {
         break;
       case 3:
         maxTentativas = MAX_TENTATIVAS_DIFICIL;
-        strcpy(palavraSecreta,
-               palavrasDificil[rand() % NUM_PALAVRAS_DIFICIL]);
+        strcpy(palavraSecreta, palavrasDificil[rand() % NUM_PALAVRAS_DIFICIL]);
         pontuacaoTotal += PONTUACAO_POR_PALAVRA_DIFICIL;
         break;
       default:
@@ -176,9 +187,11 @@ int main() {
       tentativas = 0;
       inicializarForca(palavraSecreta, letrasReveladas);
 
+      // Loop principal do jogo
       while (1) {
         ok = system("clear");
 
+        // Exibe o estado atual do jogo
         exibirForca(letrasReveladas, tentativas, maxTentativas);
 
         printf("\e[46m\e[0;95mDigite uma letra: ");
@@ -186,29 +199,37 @@ int main() {
         getchar();
         letra = toupper(letra);
 
+        // Verifica se a entrada é uma letra válida
         if (!isalpha(letra)) {
           printf("\e[46m\e[0;95mPor favor, digite uma letra válida.\n");
           continue;
         }
 
+        // Verifica se a letra está na palavra e atualiza o estado do jogo
         if (!letraEstaNaPalavra(letra, palavraSecreta, letrasReveladas)) {
           tentativas++;
         }
 
+        // Verifica se o jogador venceu
         if (strcmp(palavraSecreta, letrasReveladas) == 0) {
           ok = system("clear");
           exibirForca(letrasReveladas, tentativas, maxTentativas);
           printf("\e[46m\e[0;95mParabéns! Você acertou a palavra: %s\n", palavraSecreta);
+
+          // Solicita o nome do jogador para o ranking
           Jogador novoJogador;
-          printf("\e[46m\e[0;95mDigite seu nome para o ranking: ");
-          ok = scanf("%s", novoJogador.nome);
-          getchar();
+          printf("\e[46m\e[0;95mDigite seu nome para o ranking (limite de 5 caracteres): ");
+          ok = scanf("%5s", novoJogador.nome);
+          getchar();  // Limpar o buffer
           novoJogador.pontuacao = pontuacaoTotal;
+
+          // Adiciona o jogador ao ranking
           adicionarAoRanking(ranking, &numJogadores, novoJogador);
           esperar(2);
           break;
         }
 
+        // Verifica se o jogador perdeu
         if (tentativas == maxTentativas) {
           ok = system("clear");
           printf("\e[46m\e[0;95mVocê perdeu! A palavra era: %s\n", palavraSecreta);
@@ -219,9 +240,11 @@ int main() {
       }
       break;
     case 2:
+      // Exibe o ranking
       do {
         ok = system("clear");
 
+        // Exibe o ranking de jogadores
         exibirRanking(ranking, numJogadores);
 
         printf("\e[46m\e[0;95m+------------------------------------------------------------------------------------+\n");
